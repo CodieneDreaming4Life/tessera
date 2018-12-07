38bad7097c3770e0630677a1b9d6fbbe2b0b76b7
Mark <melowe.quorum@gmail.com>
2018-12-07 10:21:50 +0000

    Add workaround to express preference for service loader type.. In leui for fix to tighten up service loading generally.

--- shared/src/main/java/com/quorum/tessera/ServiceLoaderUtil.java
+++ shared/src/main/java/com/quorum/tessera/ServiceLoaderUtil.java
@@ -1,6 +1,8 @@
 package com.quorum.tessera;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 import java.util.ServiceLoader;
 
@@ -12,6 +14,28 @@ public interface ServiceLoaderUtil {
             return Optional.of(it.next());
         }
         return Optional.empty();
+    }
+
+    /*
+    * //WORKAROUND
+    * TODO: harden up service loader this function shouldn't be required. 
+     * Ih there are more than 1 instances return this first one.  
+     */
+    static <T> Optional<T> load(Class<T> type, Class prefered) {
+        Iterator<T> it = ServiceLoader.load(type).iterator();
+
+        List<T> all = new ArrayList<>();
+        while (it.hasNext()) {
+            T o = it.next();
+            if(prefered.isInstance(o)) {
+                return Optional.of(o);
+            }
+            
+            all.add(o);
+        }
+
+        return all.stream().findAny();
+        
     }
 
 }
--- shared/src/test/java/com/quorum/tessera/ServiceLoaderUtilTest.java
+++ shared/src/test/java/com/quorum/tessera/ServiceLoaderUtilTest.java
@@ -1,4 +1,3 @@
-
 package com.quorum.tessera;
 
 import com.acme.Bogus;
@@ -7,21 +6,39 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 
-
 public class ServiceLoaderUtilTest {
-    
+
     @Test
     public void noServiceFound() {
         Optional<ServiceLoaderUtilTest> result = ServiceLoaderUtil.load(ServiceLoaderUtilTest.class);
         assertThat(result).isNotPresent();
     }
-    
+
     @Test
     public void serviceFound() {
         Optional<Bogus> result = ServiceLoaderUtil.load(Bogus.class);
         assertThat(result).isPresent();
         assertThat(result.get()).isInstanceOf(VeryBogus.class);
-        
+
     }
-    
+
+    @Test
+    public void loadPrefered() {
+
+        Optional<Bogus> result = ServiceLoaderUtil.load(Bogus.class, VeryBogus.class);
+        assertThat(result).isPresent();
+        assertThat(result.get()).isInstanceOf(VeryBogus.class);
+    }
+
+    @Test
+    public void loadPreferedPreferenceNotFound() {
+
+        Optional<Bogus> result = ServiceLoaderUtil.load(Bogus.class, EvenMoreBogus.class);
+        assertThat(result).isPresent();
+        assertThat(result.get()).isInstanceOf(VeryBogus.class);
+    }
+
+    static class EvenMoreBogus implements Bogus {
+    }
+
 }
